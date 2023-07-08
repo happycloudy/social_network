47e67f48e447ef7428d38a909d1467d77e5a8845
happy <arion.mikolae71@gmail.com>
2023-07-08 23:34:05 +0300

    add user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {User} from "./user/user.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'user',
+            password: 'password',
+            database: 'social_network',
+            entities: [User],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
